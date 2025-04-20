bcbe9aa4fe5896949607d9fbff87ba657b32e147
mochadwi <iqbal@bwc.ae>
2025-04-20 13:40:59 +0700

    feat: Initial Commit

---
+++ app/src/main/java/io/mochadwi/spacenews/SpacePulseApp.kt
@@ -0,0 +1,9 @@
+package io.mochadwi.spacenews
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpacePulseApp : Application() {
+    // Application-level setup can go here if needed later
+}
